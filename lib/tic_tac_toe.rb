463a13f3814646915bd0e44606760fd5038d5d17
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-06-28 04:18:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ def turn(board)
   index = input_to_index(input)
 
 if valid_move?(board, index)
-  move(board, index, value = "X")
+  move(board, index, value)
   display_board(board)
 else
   turn(board)
